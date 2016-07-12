474c6df1e61f4895f3c1807e1d0d3075e3a684ae
Justin Cope <justin.cope@utexas.edu>
2016-07-12 13:52:11 -0500

    Add files via upload

---
+++ timebank.R
@@ -0,0 +1,181 @@
+
+
+library(XML)
+
+####### FUNCTIONS #######
+
+getIndex = function(index_file){
+	strsplit(readLines(index_file), " ")[[1]]
+}
+ 
+getDocument = function(file){
+	xmlRoot(xmlTreeParse(file, useInternal = TRUE))
+} # useInternal maintains the xml document, for use with XPATH searches
+
+getDocuments = function(index){
+	n = length(index)
+	x = vector("list",n)
+	if(n>0){
+		for(i in 1:n){
+			x[[i]] = getDocument(index[[i]])
+			id = paste(c("d",i), collapse="")
+			xmlAttrs(x[[i]]) <- c(DocID=id)
+		}
+	return(x)
+	}
+}
+
+UniqueXID = function(timeml_node, id_name){
+	dID = xmlGetAttr(timeml_node,"DocID")
+	search = paste("//*[@", id_name, "]", sep="")
+	hits = getNodeSet(timeml_node,search)
+	n = length(hits)
+	if(n>0){
+	for(i in 1:n){
+		value = xmlGetAttr(hits[[i]],id_name)
+		insert = paste(dID, value, sep="")
+		xmlAttrs(hits[[i]])[[id_name]] = insert
+	}}
+}
+
+makeUnique = function(doc){
+	dID = xmlGetAttr(doc,"DocID")
+	ids = c("eid", "eiid", "eventID", "tid", "sid", "lid", "timeID", "signalID", "eventInstanceID", "subordinatedEventInstance", "relatedToTime", "relatedToEventInstance")
+	n = length(ids)
+	for(i in 1:n){
+		UniqueXID(doc,ids[[i]])
+	}
+}
+
+makeAllDocsUnique = function(corpus){
+	n = length(corpus)
+	for(i in 1:n){
+		makeUnique(corpus[[i]])
+	}
+}
+
+
+##########
+
+getSentences = function(doc){
+	getNodeSet(doc,"//s")
+}
+
+printSentence = function(sentence){
+	gsub("\\n"," ",xmlValue(sentence))
+}
+
+printDocument = function(document){
+	paste(lapply(getSentences(document),printSentence), collapse=" | ")
+}
+
+getEvents = function(doc_or_sentence){
+	getNodeSet(doc_or_sentence,"//EVENT")
+}
+
+getTimes = function(doc_or_sentence){
+	getNodeSet(doc_or_sentence,"//TIMEX3")
+}
+
+getSignals = function(doc_or_sentence){
+	getNodeSet(doc_or_sentence,"//SIGNAL")
+}
+
+getEventInstances = function(top,event){ # Takes an input of type EVENT
+	eid = xmlGetAttr(event,"eid")
+	# builds a search for MAKEINSTANCE objects with attribute eventID equal to input
+	search = paste("//MAKEINSTANCE[@eventID='", eid, "']",sep="") 
+	getNodeSet(top,search) # executes search, returning hits
+}
+
+getSentenceEventInstances = function(sentence){
+	x = getEvents(sentence)
+	y = length(x)
+	if(y>0){
+		z <- vector("list", y)
+		for(i in 1:y){
+			a = getEventInstances(sentence,x[[i]])
+			z[[i]] = c(x[[i]],a)
+		}
+		return(z)
+	}
+}
+
+frameInstances = function(event_instance_list){
+	x = length(event_instance_list)
+	y = vector("list",x)
+	for(i in 1:x){
+		y[[i]] = event_instance_list[[i]][[2]]
+	}
+	sapply(y,xmlAttrs)
+}
+
+
+##########
+
+
+getSentenceEntities = function(sentences){
+	if(length(sentences) > 0){
+		for(i in 1:length(sentences)){ 
+			sentence = sentences[[i]]
+			print(sentence)
+			events = getNodeSet(sentence,"EVENT")
+			if(length(events) > 0){
+				for(j in 1:length(events)){
+					print(events[[j]])
+					eid = xmlGetAttr(events[[j]],"eid")
+					print(eid)
+					getEventInstances(eid)
+				}
+			}
+		}
+	}
+	else print("No sentences")
+}
+
+
+
+
+####### PROGRAM ##########
+
+
+index = getIndex("index.txt")
+documents = getDocuments(index)
+
+makeAllDocsUnique(documents)
+
+docs = newXMLNode("documents")
+addChildren(docs,documents)
+
+
+test = getNodeSet(docs,"//MAKEINSTANCE")
+d = sapply(test,xmlAttrs) #this is just a list
+
+n = length(d)
+r = d[[1]]
+for(i in 2:n){
+	r = rbind(r,d[[i]])
+} #now we have a data frame 
+
+
+getNodeSet(docs[[1]],"//*[@*='ei426']")
+
+plurals = getNodeSet(docs,"//MAKEINSTANCE[@pos='PLURAL']")
+
+
+sentences = getNodeSet(top,"//s")
+xmlSApply(sentences, function(x) xmlSApply(x,xmlValue))
+
+nodes = getNodeSet(top,"//MAKEINSTANCE[@tense]")
+lapply(nodes, function (x) xmlSApply(x,xmlValue))
+
+
+# xmlName([node]) # self
+# names([node]) # children
+
+
+
+
+
+
+
